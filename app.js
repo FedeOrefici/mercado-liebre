5a8bde1e22ec67c6730aca10e8915b1707937b29
FedeOrefici <federicoaorefici@gmail.com>
2022-06-19 12:19:53 +0200

    mercado liebre

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+//middleware
+app.use(express.static('public'));
+app.set('puerto', process.env.PORT || 3001);
+
+//rutas
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+// app.listen(app.get('puerto')), ()=> console.log(`'server working' ${app.get('puerto')}`);
+
+app.listen(3000, ()=> console.log('server working'));
